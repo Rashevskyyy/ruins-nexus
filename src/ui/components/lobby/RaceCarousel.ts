fe2c62f0f80798e2e12276cb299034663cd5d83f
Rashevskyyy <80958061+Rashevskyyy@users.noreply.github.com>
2026-09-19 01:13:01 +0200

    Fix lobby race selection and stop animations on destroyed buttons

--- src/ui/components/lobby/RaceCarousel.ts
+++ src/ui/components/lobby/RaceCarousel.ts
@@ -325,6 +325,7 @@ export class RaceCarousel extends PIXI.Container {
         button.on('pointerdown', () => {
             this.selectedOption = option;
             this.updateOptionButtons();
+            this.selectCurrentRace();
         });
 
         button.on('pointerover', () => {
